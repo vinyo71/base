b05de883665acf7d29a5e35f5712e5fd4c0f4c07
vinyo71 <60270462+vinyo71@users.noreply.github.com>
2021-03-29 21:12:37 +0200

    fixed tachograpgh

--- train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
@@ -2,13 +2,26 @@ package hu.bme.mit.train.tachograph;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
-
 import java.util.Arrays;
+
+import hu.bme.mit.train.user.TrainUserImpl;
+import hu.bme.mit.train.controller.TrainControllerImpl;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
+
 
 
 public class TrainTachograph {
-    Table<String, Integer, Double> tachograph = HashBasedTable.create();
+    private TrainUser user = new TrainUserImpl(controller);
+    private TrainController controller = new TrainControllerImpl();
+    private Table<String, Integer, Double> tachograph;
 
+    public TrainTachograph() {
+        tachograph = HashBasedTable.create();
+        table.put(java.time.LocalTime.now(), user.getJoystickPosition(), controller.getReferenceSpeed());
+    }
+    
     public void put(String time, Integer joyPos, Double refSpeed){
         tachograph.put(time, joyPos, refSpeed);
     }
